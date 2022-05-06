42d40011c08c4824008e66dbd2e7e0c2c465b21d
Preeti <preetitrikha26@yahoo.com>
2022-05-06 19:14:59 +0530

    Modified

--- weather.js
+++ weather.js
@@ -1,23 +1,23 @@
 
 
 button=document.getElementById("Enter");
-button.addEventListener("click", function (){
+button.addEventListener("click", function (e){
     let city=document.getElementById("city").value;
     let p=/^[A-za-z]*$/g;
     if(city!="" && p.test(city)){
         let k="";
-        let m="metric"
+        var m="metric";
         let url= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+k+"&units="+m;
         fetch(url).then((response)=>{
             return response.json();
         }).then((data)=>{
             let n=data['name'];
             let t=data.main.temp;
-            let mt=data.main.temp_min;
-            let maxt=data.main.temp_max;
             let pre=data.main.pressure;
             let humi=data.main.humidity;
-            document.getElementById("result").innerHTML=n+" : "+t+" Celsius <br> Min Temp: "+mt+" <br> Max Temp: "+maxt+"<br> Pressure: "+pre+"<br> Humidity: "+humi;
+            let d=data.weather;
+            let ws=data.wind.speed;
+            document.getElementById("res").innerHTML="<b> WEATHER REPORT: </b> <BR> "+n+" : "+t+" Celsius <br> Pressure: "+pre+"<br> Humidity: "+humi+"<br> Description: "+d[0].description+"<br> Wind Speed: "+ws+" m/s ";
         }).catch((err)=>{alert("Error: City Not Found")});
     }
     else{
